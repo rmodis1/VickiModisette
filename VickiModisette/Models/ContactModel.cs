8d6de8adcf7a49e8e862afe4f816bae47b3deee8
Ryan Modisette <modisetteryan@gmail.com>
2024-07-08 15:02:37 -0400

    Added ContactModel class

---
+++ VickiModisette/Models/ContactModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace VickiModisette.Models
+{
+	public class ContactModel
+	{
+		public int ClientId { get; set; }
+		[Required]
+		public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; } = string.Empty;
+		public DateTime TimeSubmitted { get; set; }
+	}
+}
+
